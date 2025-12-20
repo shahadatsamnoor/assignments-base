caf5cbdb67471d7f9055a7effd2a8f2f13764271
shahadat559 <shahadat@theplusnation.com>
2025-12-19 22:42:51 -0500

    assignment1 submission

---
+++ finder-app/finder.sh
@@ -0,0 +1,27 @@
+if [ $# -lt 2 ]
+then
+    echo "filesdir or searchstr was not specified."
+    exit 1
+fi
+
+echo $1 | grep -qE "/$"
+if [ $? -eq 0 ]
+then
+    filesdir=$1
+else
+    filesdir=$1/
+fi
+
+searchstr=$2
+
+if ! [ -d $filesdir ]
+then
+    echo "$filesdir does not represent a directory on the filesystem"
+    exit 1
+fi
+
+X=$(grep -rl $searchstr $filesdir | wc -l )
+
+Y=$(grep -r $searchstr $filesdir | wc -l )
+
+echo "The number of files are"$X" and the number of matching lines are"$Y
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,11 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+
+    const char *c_username = my_username();
+    char *username = malloc_username_from_conf_file();
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(c_username, username, "c_username and username does not match");
+    free(username);
 }
